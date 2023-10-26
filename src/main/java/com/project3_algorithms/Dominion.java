497d90b93d6438413a1dd59bf332062342635348
jacobobendrado <jacobobendrado@gmail.com>
2023-10-26 16:17:31 -0400

    Dominion

--- src/main/java/com/project3_algorithms/Dominion.java
+++ src/main/java/com/project3_algorithms/Dominion.java
@@ -6,10 +6,47 @@ package com.project3_algorithms;
  */
 public class Dominion {
     
-    private int i;
+    private int n, m, k;
+    private Dominion representative;
+    private Dominion parent;
+    private int rank;
     
-    public Dominion() {
-        
+    public Dominion(int nn, int mm, int kk) {
+        n = nn;
+        m = mm;
+        k = kk;
+        parent = null;
+        representative = null;
+        rank = 0;
+    }
+    
+    public int[] getPosition() {
+        return new int[] {n, m, k};
+    }
+    
+    public Dominion getRepresentative() {
+        return compressPath();
+    }
+    
+    public int getRank() {
+        return rank;
+    }
+    
+    public void setRepresentative(Dominion newRep) {
+        compressPath().parent = newRep;
+    }
+    
+    public void setRank(int newRank) {
+        rank = newRank;
+    }
+    
+    
+    private Dominion compressPath() {
+        if (parent != null && parent != this) {
+            parent = parent.compressPath();
+            representative = parent;
+        }
+        return parent;
     }
 }
 
